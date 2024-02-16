3949f3ca46421e58bcd0c86c32f4e60c9a598ea5
fabianLaske <139952454+fabianLaske@users.noreply.github.com>
2024-02-16 11:14:57 +0100

    fix

--- src/QuestGame.js
+++ src/QuestGame.js
@@ -1,14 +1,120 @@
 // QuestGame.js
 import React, { useState, useEffect } from 'react';
 import Task from './Task';
-import piratBild from './img/pirat.png'; 
+import piratBild from './img/pirat.png';
 
 const QuestGame = () => {
   const [currentTask, setCurrentTask] = useState(2);
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSucessMessage] = useState("");
   const [queryTableData, setQueryTableData] = useState([]);
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      question: 'Wir sind gestrandet, wir sollten die anderen Mitglieder der Crew suchen.',
+      tipp: 'db.crew.find({})',
+      expectedQuery: 'db.crew.find({})',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 2,
+      question: 'Ohje ich bin noch neu auf dem Schiff und weiß nicht wer den Beruf Capitän hat',
+      tipp: 'db.crew.find({})',
+      expectedQuery: 'db.crew.find({Beruf: "Capitän"})',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 3,
+      question: 'Gut das du mich aus den Trümmern finden konntest, hust hust. Trommle alle Leute zusammen die unter 42 Jahre alt sind. Wir müssen die Insel erkunden',
+      tipp: 'db.crew.find({})',
+      expectedQuery: 'db.items.find({ age: { $lt: 42 } })',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 4,
+      question: 'Aber Capitän das schließt sie aber aus, dass finde ich etwas unfair',
+      tipp: 'db.crew.find({})',
+      expectedQuery: 'db.items.find({$or: [{ age: { $lt: 42 } },{ Beruf: "Capitän" }]})',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 5,
+      question: 'Nagut ok ich komme mit wenn du mein älteres Exemplar meines Buch "Naturkunde für Dummis" findest',
+      tipp: 'db.books.find({}).sort({}).limit(1)',
+      expectedQuery: 'db.books.find({ Titel: "Naturkunde für Dummis", Bestitzer: "Capitän" }).sort({ Erstellungsdatum: 1 }).limit(1)',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 6,
+      question: 'Füge das Buch nun deinem Inventar zu.',
+      tipp: 'db.books.insertOne({name: db.books.find({...).sort(...).limit(1).next().Titel});',
+      expectedQuery: 'db.stash.insertOne({name: db.books.find({ Titel: "Naturkunde für Dummies", Bestitzer: "Capitän" }).sort({ Erstellungsdatum: 1 }).limit(1).next().Titel});',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 7,
+      question: 'Die verbleibenden auf dem Schiff sollen eine Party feiern. Wie viel Alohol haben wir noch an bord?',
+      tipp: 'db.items.find({ category: "Alkohol" })',
+      expectedQuery: 'db.items.find({ category: "Alkohol" })',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 8,
+      question: 'Oh, dann sind ein paar abgelaufene Sachen dabei, schmeiße alle Flasche mit einem Verfalldatum nach dem 1. Januar 2024 weg.',
+      tipp: 'db.items.deleteMany({ creation_date: { $lt: ISODate("2024-01-01") } })',
+      expectedQuery: 'db.items.deleteMany({ creation_date: { $lt: ISODate("2024-01-01") } })',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 9,
+      question: 'Verringere in der Zwischenzeit die Preis für Alkohol um 10%, nur um eine Meuterrei verzubeugen.',
+      tipp: 'db.items.updateMany({ category: "Alkohol" }, { $mul: { price: 1.1 } })',
+      expectedQuery: 'db.items.updateMany({ category: "Alkohol" }, { $mul: { price: 1.1 } })',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 10,
+      question: 'Was für Gegenstände haben wir auf dem Schiff?',
+      tipp: 'db.items.find({})',
+      expectedQuery: 'db.items.find({})',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 11,
+      question: 'Dass sind aber viele, kannst du mir nur die Waffen zeigen?',
+      tipp: 'db.items.find({ usage: "Waffe" })',
+      expectedQuery: 'db.items.find({ usage: "Waffe" })',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 13,
+      question: 'Berechne den Durchschnittspreis aller Produkte.',
+      tipp: 'db.items.aggregate([{ $group: { _id: null, averagePrice: { $avg: "$price" } } }])',
+      expectedQuery: 'db.items.aggregate([{ $group: { _id: null, averagePrice: { $avg: "$price" } } }])',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+    {
+      id: 14,
+      question: 'Erstelle einen Index für das Feld "Datum" in der Sammlung "Protokolle".',
+      tipp: 'db.logs.createIndex({ date: 1 })',
+      expectedQuery: 'db.logs.createIndex({ date: 1 })',
+      error: "das war flasch",
+      success: "du hast es geschafft!"
+    },
+  ])
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,7 +135,7 @@ const QuestGame = () => {
   const handleQuerySubmit = async (query) => {
     const fomattedQuerry = query.replace(/\s/g, "").replace(/['`"]/g, "'")
     const formattedExpectedQuery = tasks[currentTask - 1].expectedQuery.replace(/\s/g, "").replace(/['`"]/g, "'")
-    if ( formattedExpectedQuery === fomattedQuerry  
+    if ( formattedExpectedQuery === fomattedQuerry
     ) {
       setCurrentTask(currentTask + 1);
       setErrorMessage("");
@@ -44,7 +150,7 @@ const QuestGame = () => {
           },
           body: JSON.stringify({ query }),
         };
-    
+
         const response = await fetch('http://localhost:3001/api/query', requestOptions);
         const data = await response.json();
         setQueryTableData(data);
@@ -55,7 +161,7 @@ const QuestGame = () => {
       setErrorMessage(tasks[currentTask - 1].error);
     }
   };
-  
+
 
   return (
     <div>
@@ -107,7 +213,7 @@ const QuestGame = () => {
         </div>
     </div>
     </div>
-  
+
   );
 };
 
--- src/Task.js
+++ src/Task.js
@@ -1,84 +1,87 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-const Task = ({ task, onQuerySubmit }) => {
-  const [query, setQuery] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
+const Task = ({task, onQuerySubmit}) => {
+    const [query, setQuery] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
 
-  const handleInputChange = (e) => {
-    const userInput = e.target.value;
+    const handleInputChange = (e) => {
+        const userInput = e.target.value;
 
-    if (userInput.includes(".")) {
-      const lastDotIndex = userInput.lastIndexOf(".");
-      const searchQuery = userInput.slice(lastDotIndex + 1);
-      setQuery(userInput);
-      setSuggestions(getMatchingSuggestions(searchQuery));
-    } else {
-      setQuery(userInput);
+        if (userInput.includes(".")) {
+            setQuery(userInput);
+            setSuggestions(getMatchingSuggestions(userInput));
+        } else {
+            setQuery(userInput);
 
-      const matchingSuggestions = getMatchingSuggestions(userInput);
-      setSuggestions(matchingSuggestions);
-    }
-  };
+            const matchingSuggestions = getMatchingSuggestions(userInput);
+            setSuggestions(matchingSuggestions);
+        }
+    };
 
-  const handleSuggestionClick = (suggestion) => {
+    const handleSuggestionClick = (suggestion) => {
 
-    const lastDotIndex = query.lastIndexOf(".");
-    const newQuery = query.slice(0, lastDotIndex + 1) + suggestion;
-    setQuery(newQuery);
-    setSuggestions([]);
-  };
-
-  const getMatchingSuggestions = (input) => {
-        // let staticSuggestions = ['db.'];
-
-    // if (input.includes("db.")) {
-    //   console.log(input);
-    //   staticSuggestions = ['crew.', 'items.', 'stash'];
-
-    // }
-    //   return staticSuggestions.filter((suggestion) => suggestion.toLowerCase().includes(input.toLowerCase())
-    // );
-    let staticSuggestions = ['db.','crew.', 'items.', 'stash.' ,'find({})', 'aggregate({})'];
-
-    return staticSuggestions.filter((suggestion) => suggestion.toLowerCase().includes(input.toLowerCase())
-    );
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      if (suggestions.length > 0) {
-        // Wenn es Suggestions gibt, wähle die erste Suggestion aus
-        const closestSuggestion = suggestions[0];
         const lastDotIndex = query.lastIndexOf(".");
-        setQuery((prevQuery) => prevQuery.slice(0, lastDotIndex + 1) + closestSuggestion);
-        setSuggestions([]); // Schließe die Dropdown-Liste
-      }
-      onQuerySubmit(query); // Rufe onQuerySubmit mit dem aktualisierten Query-Wert auf
-    }
-  };
+        const newQuery = query.slice(0, lastDotIndex + 1) + suggestion;
+        setQuery(newQuery);
+        setSuggestions([]);
+    };
 
-  return (
-    <div>
-      <h2>Aufgabe {task.id}</h2>
-      <p>{task.question}</p>
-      <input
-        type="text"
-        value={query}
-        onChange={handleInputChange}
-        placeholder={task.tipp}
-        onKeyDown={handleKeyPress}
-      />
-      {suggestions.length > 0 && (
-        <ul>
-          {suggestions.map((suggestion, index) => (
-            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
-      <button onClick={() => onQuerySubmit(query)}>Einreichen</button>
-    </div>
-  );
+    const getMatchingSuggestions = (input) => {
+        let staticSuggestions = ['db.'];
+
+        if (input.includes("db.")) {
+            console.log(input);
+            staticSuggestions = ['crew.', 'items.', 'stash'];
+
+        }
+        console.log({staticSuggestions})
+
+        const lastDotIndex = input.lastIndexOf(".");
+        const lastWord = input.slice(lastDotIndex + 1);
+
+        return staticSuggestions.filter((suggestion) => suggestion.toLowerCase().includes(lastWord.toLowerCase())
+        );
+        // let staticSuggestions = ['db.','crew.', 'items.', 'stash.' ,'find({})', 'aggregate({})'];
+        //
+        // return staticSuggestions.filter((suggestion) => suggestion.toLowerCase().includes(input.toLowerCase())
+        // );
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            if (suggestions.length > 0) {
+                // Wenn es Suggestions gibt, wähle die erste Suggestion aus
+                const closestSuggestion = suggestions[0];
+                const lastDotIndex = query.lastIndexOf(".");
+                setQuery((prevQuery) => prevQuery.slice(0, lastDotIndex + 1) + closestSuggestion);
+                setSuggestions([]); // Schließe die Dropdown-Liste
+            }
+            onQuerySubmit(query); // Rufe onQuerySubmit mit dem aktualisierten Query-Wert auf
+        }
+    };
+
+    return (
+        <div>
+            <h2>Aufgabe {task.id}</h2>
+            <p>{task.question}</p>
+            <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+                placeholder={task.tipp}
+                onKeyDown={handleKeyPress}
+            />
+            {suggestions.length > 0 && (
+                <ul>
+                    {suggestions.map((suggestion, index) => (
+                        <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                            {suggestion}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <button onClick={() => onQuerySubmit(query)}>Einreichen</button>
+        </div>
+    );
 };
 export default Task;
